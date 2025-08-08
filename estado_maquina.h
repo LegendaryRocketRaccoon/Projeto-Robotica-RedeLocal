fe9af1d1c6520638106847c41e48912eafc802bc
Gustavo Chimello <chimellogustavo17@gmail.com>
2025-08-08 08:07:21 -0300

    Add files via upload

---
+++ estado_maquina.h
@@ -0,0 +1,10 @@
+#ifndef ESTADO_MAQUINA_H
+#define ESTADO_MAQUINA_H
+
+#include "estados.h"
+
+void atualizarEstado();
+
+extern Estado estadoAtualAnterior;
+
+#endif
---
+++ servidor.h
@@ -0,0 +1,10 @@
+#ifndef SERVIDOR_H
+#define SERVIDOR_H
+
+#include <WebServer.h>
+
+extern WebServer server;
+
+void configurarServidor();
+
+#endif
